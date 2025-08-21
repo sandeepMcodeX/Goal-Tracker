d9f8c2e25f8d88fd824b1dd9f90b680bd73b6ecc
sandeep <sandeepmcodex.dev@gmail.com>
2025-08-21 22:31:34 +0530

    Add files via upload

---
+++ Goal Tracker/script.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+const inputBoxOne = document.querySelector('.input-one');
+const inputBoxTwo = document.querySelector('.input-one');
+const inputBoxThree = document.querySelector('.input-one');
+
+form.addEventListener('click', (event)=>{
+  event.preventDefault();
+  const inputBoxOneValue  = inputBoxOne.value;
+  const inputBoxTwoValue  = inputBoxTwo.value;
+  const inputBoxThreeValue  = inputBoxTwo.value;
+
+
+  
+})
